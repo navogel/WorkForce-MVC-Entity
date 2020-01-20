96a9834b49a0d008a97cc4380144da678f530c27
Nate V <navogel@gmail.com>
2020-01-20 15:12:14 -0600

    second commit with sln.

---
+++ Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bangazon_RedJags.Models
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(55, MinimumLength = 1, ErrorMessage = "First Name length should be between 1 and 55 characters")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(55, MinimumLength = 1, ErrorMessage = "Last Name length should be between 1 and 55 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Department Id is required")]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public Computer Computer { get; set; }
+
+       
+        [StringLength(55, MinimumLength = 5, ErrorMessage = "eMail length should be between 5 and 55 characters")]
+        public string Email { get; set; }
+
+        
+        [Display(Name = "Is supervisor")]
+        public bool IsSupervisor { get; set; } = false;
+
+        
+        [Display(Name = "Computer")]
+        public int ComputerId { get; set; } = -1;
+
+        
+
+
+
+    }
+}
---
+++ Models/computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_RedJags.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Purchase Date is required")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Purchase Date")]
+        public DateTime PurchaseDate { get; set; }
+        [Display(Name = "Decommission Date")]
+        public DateTime? DecomissionDate { get; set; } = null;
+
+        [Required(ErrorMessage = "Computer Manufacturer is required")]
+        [Display(Name = "Manufacturer")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Manufacturer Name length should be between 1 and 20 characters")]
+        public string Make { get; set; }
+        [Display(Name = "Model")]
+        [Required(ErrorMessage = "Computer Model is required")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Model Name length should be between 1 and 20 characters")]
+        public string Model { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
